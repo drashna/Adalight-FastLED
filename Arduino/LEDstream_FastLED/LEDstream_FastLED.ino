3be50084b7db99af2b86ea488a0104e86a9b46fc
Drashna Jael're <drashna@live.com>
2020-12-31 12:01:19 -0800

    Add static animation

--- Arduino/LEDstream_FastLED/LEDstream_FastLED.ino
+++ Arduino/LEDstream_FastLED/LEDstream_FastLED.ino
@@ -1,9 +1,11 @@
-/*
- *  Project     Adalight FastLED
- *  @author     David Madison
- *  @link       github.com/dmadison/Adalight-FastLED
- *  @license    LGPL - Copyright (c) 2017 David Madison
+/* LEDstream_FastLED
+ * 
+ * Modified version of Adalight that uses the FastLED
+ * library (http://fastled.io) for driving led strips.
+ * 
+ * http://github.com/dmadison/Adalight-FastLED
  *
+ * --------------------------------------------------------------------
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -16,38 +18,36 @@
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
+ * --------------------------------------------------------------------
  */
 
-#include <Arduino.h>
-
 // --- General Settings
-
-const uint16_t 
-	Num_Leds   =  80;         // strip length
-const uint8_t
-	Brightness =  255;        // maximum brightness
+static const uint16_t 
+  Num_Leds   =  63;        // strip lengthh
+static const uint8_t
+  Brightness =  255;       // maximum brightness
 
 // --- FastLED Setings
-#define LED_TYPE     WS2812B  // led strip type for FastLED
-#define COLOR_ORDER  GRB      // color order for bitbang
-#define PIN_DATA     1        // led data output pin
-// #define PIN_CLOCK  7       // led data clock pin (uncomment if you're using a 4-wire LED type)
+#define LED_TYPE     WS2812B // led strip type for FastLED
+#define COLOR_ORDER  GRB     // color order for bitbang
+#define PIN_DATA     1       // led data output pin
+//#define PIN_CLOCK  7       // led data clock pin (uncomment if you're using a 4-wire LED type)
 
 // --- Serial Settings
-const unsigned long
-	SerialSpeed    = 115200;  // serial port speed
-const uint16_t
-	SerialTimeout  = 60;      // time before LEDs are shut off if no data (in seconds), 0 to disable
+static const unsigned long
+  SerialSpeed    = 921600; // serial port speed
+static const uint16_t
+  SerialTimeout  = 900;     // time before LEDs are shut off if no data (in seconds), 0 to disable
 
 // --- Optional Settings (uncomment to add)
-#define SERIAL_FLUSH          // Serial buffer cleared on LED latch
-// #define CLEAR_ON_START     // LEDs are cleared on reset
-//#define GROUND_PIN 10      // additional grounding pin (optional)
+#define SERIAL_FLUSH         // Serial buffer cleared on LED latch
+#define CLEAR_ON_START     // LEDs are cleared on reset
+#define GROUND_PIN 10      // additional grounding pin (optional)
+//#define CALIBRATE          // sets all LEDs to the color of the first
 
 // --- Debug Settings (uncomment to add)
-// #define DEBUG_LED 13       // toggles the Arduino's built-in LED on header match
-// #define DEBUG_FPS 8        // enables a pulse on LED latch
+//#define DEBUG_LED 13       // toggles the Arduino's built-in LED on header match
+//#define DEBUG_FPS 8        // enables a pulse on LED latch
 
 // --------------------------------------------------------------------
 
@@ -70,8 +70,8 @@ uint8_t * ledsRaw = (uint8_t *)leds;
 // XOR 0x55). LED data follows, 3 bytes per LED, in order R, G, B,
 // where 0 = off and 255 = max brightness.
 
-const uint8_t magic[] = {
-	'A','d','a'};
+static const uint8_t magic[] = {
+  'A','d','a'};
 #define MAGICSIZE  sizeof(magic)
 
 // Check values are header byte # - 1, as they are indexed from 0
@@ -81,158 +81,255 @@ const uint8_t magic[] = {
 
 enum processModes_t {Header, Data} mode = Header;
 
-int16_t c;  // current byte, must support -1 if no data available
-uint16_t outPos;  // current byte index in the LED array
-uint32_t bytesRemaining;  // count of bytes yet received, set by checksum
-unsigned long t, lastByteTime, lastAckTime;  // millisecond timestamps
+static int16_t
+  c;
+static uint16_t
+  outPos;
+static uint32_t
+  bytesRemaining;
+static unsigned long
+  t,
+  lastByteTime,
+  lastAckTime;
 
-void headerMode();
-void dataMode();
-void timeouts();
+static bool idle_animation;
 
-// Macros initialized
-#ifdef SERIAL_FLUSH
-	#undef SERIAL_FLUSH
-	#define SERIAL_FLUSH while(Serial.available() > 0) { Serial.read(); }
-#else
-	#define SERIAL_FLUSH
-#endif
-
+// Debug macros initialized
 #ifdef DEBUG_LED
-	#define ON  1
-	#define OFF 0
+  #define ON  1
+  #define OFF 0
 
-	#define D_LED(x) do {digitalWrite(DEBUG_LED, x);} while(0)
+  #define D_LED(x) do {digitalWrite(DEBUG_LED, x);} while(0)
 #else
-	#define D_LED(x)
+  #define D_LED(x)
 #endif
 
 #ifdef DEBUG_FPS
-	#define D_FPS do {digitalWrite(DEBUG_FPS, HIGH); digitalWrite(DEBUG_FPS, LOW);} while (0)
+  #define D_FPS do {digitalWrite(DEBUG_FPS, HIGH); digitalWrite(DEBUG_FPS, LOW);} while (0)
 #else
-	#define D_FPS
+  #define D_FPS
 #endif
 
+uint8_t maxChanges = 24;      // Value for blending between palettes.
+ 
+CRGBPalette16 currentPalette;
+CRGBPalette16 targetPalette;
+TBlendType    currentBlending;                                // NOBLEND or LINEARBLEND 
+
+uint16_t Xorig = 0x012;
+uint16_t Yorig = 0x015;
+uint16_t X;
+uint16_t Y;
+uint16_t Xn;
+uint16_t Yn;
+uint8_t index;
+
 void setup(){
-	#ifdef DEBUG_LED
-		pinMode(DEBUG_LED, OUTPUT);
-		digitalWrite(DEBUG_LED, LOW);
-	#endif
+  #ifdef GROUND_PIN
+    pinMode(GROUND_PIN, OUTPUT);
+    digitalWrite(GROUND_PIN, LOW);
+  #endif
 
-	#ifdef DEBUG_FPS
-		pinMode(DEBUG_FPS, OUTPUT);
-	#endif
+  #ifdef DEBUG_LED
+    pinMode(DEBUG_LED, OUTPUT);
+    digitalWrite(DEBUG_LED, LOW);
+  #endif
 
-	#if defined(PIN_CLOCK) && defined(PIN_DATA)
-		FastLED.addLeds<LED_TYPE, PIN_DATA, PIN_CLOCK, COLOR_ORDER>(leds, Num_Leds);
-	#elif defined(PIN_DATA)
-		FastLED.addLeds<LED_TYPE, PIN_DATA, COLOR_ORDER>(leds, Num_Leds);
-	#else
-		#error "No LED output pins defined. Check your settings at the top."
-	#endif
-	
-	FastLED.setBrightness(Brightness);
+  #ifdef DEBUG_FPS
+    pinMode(DEBUG_FPS, OUTPUT);
+  #endif
 
-	#ifdef CLEAR_ON_START
-		FastLED.show();
-	#endif
+  #ifdef PIN_CLOCK
+    FastLED.addLeds<LED_TYPE, PIN_DATA, PIN_CLOCK, COLOR_ORDER>(leds, Num_Leds);
+  #else
+    FastLED.addLeds<LED_TYPE, PIN_DATA, COLOR_ORDER>(leds, Num_Leds);
+  #endif
+  
+  FastLED.setBrightness(Brightness);
 
-	Serial.begin(SerialSpeed);
-	Serial.print("Ada\n"); // Send ACK string to host
+  #ifdef CLEAR_ON_START
+    FastLED.show();
+  #endif
 
-	lastByteTime = lastAckTime = millis(); // Set initial counters
+  Serial.begin(SerialSpeed);
+  Serial.print("Ada\n"); // Send ACK string to host
+
+  lastByteTime = lastAckTime = millis(); // Set initial counters
+
+  
+  currentBlending = LINEARBLEND;
+
+  X=Xorig;
+  Y=Yorig;
+  idle_animation = true;
 }
 
-void loop(){ 
-	t = millis(); // Save current time
+void loop(){
+  adalight();
+}
 
-	// If there is new serial data
-	if((c = Serial.read()) >= 0){
-		lastByteTime = lastAckTime = t; // Reset timeout counters
+void adalight(){ 
+  t = millis(); // Save current time
 
-		switch(mode) {
-			case Header:
-				headerMode();
-				break;
-			case Data:
-				dataMode();
-				break;
-		}
-	}
-	else {
-		// No new data
-		timeouts();
-	}
+  // If there is new serial data
+  if((c = Serial.read()) >= 0){
+    lastByteTime = lastAckTime = t; // Reset timeout counters
+    idle_animation = false;
+ 
+    switch(mode) {
+      case Header:
+        headerMode();
+        break;
+      case Data:
+        dataMode();
+        break;
+    }
+  }
+  else {
+    // No new data
+    timeouts();
+  }
+  if (idle_animation){ ser_task(); }
 }
 
 void headerMode(){
-	static uint8_t
-		headPos,
-		hi, lo, chk;
+  static uint8_t
+    headPos,
+    hi, lo, chk;
 
-	if(headPos < MAGICSIZE){
-		// Check if magic word matches
-		if(c == magic[headPos]) {headPos++;}
-		else {headPos = 0;}
-	}
-	else{
-		// Magic word matches! Now verify checksum
-		switch(headPos){
-			case HICHECK:
-				hi = c;
-				headPos++;
-				break;
-			case LOCHECK:
-				lo = c;
-				headPos++;
-				break;
-			case CHECKSUM:
-				chk = c;
-				if(chk == (hi ^ lo ^ 0x55)) {
-					// Checksum looks valid. Get 16-bit LED count, add 1
-					// (# LEDs is always > 0) and multiply by 3 for R,G,B.
-					D_LED(ON);
-					bytesRemaining = 3L * (256L * (long)hi + (long)lo + 1L);
-					outPos = 0;
-					memset(leds, 0, Num_Leds * sizeof(struct CRGB));
-					mode = Data; // Proceed to latch wait mode
-				}
-				headPos = 0; // Reset header position regardless of checksum result
-				break;
-		}
-	}
+  if(headPos < MAGICSIZE){
+    // Check if magic word matches
+    if(c == magic[headPos]) {headPos++;}
+    else {headPos = 0;}
+  }
+  else{
+    // Magic word matches! Now verify checksum
+    switch(headPos){
+      case HICHECK:
+        hi = c;
+        headPos++;
+        break;
+      case LOCHECK:
+        lo = c;
+        headPos++;
+        break;
+      case CHECKSUM:
+        chk = c;
+        if(chk == (hi ^ lo ^ 0x55)) {
+          // Checksum looks valid. Get 16-bit LED count, add 1
+          // (# LEDs is always > 0) and multiply by 3 for R,G,B.
+          D_LED(ON);
+          bytesRemaining = 3L * (256L * (long)hi + (long)lo + 1L);
+          outPos = 0;
+          memset(leds, 0, Num_Leds * sizeof(struct CRGB));
+          mode = Data; // Proceed to latch wait mode
+        }
+        headPos = 0; // Reset header position regardless of checksum result
+        break;
+    }
+  }
 }
 
 void dataMode(){
-	// If LED data is not full
-	if (outPos < sizeof(leds)){
-		ledsRaw[outPos++] = c; // Issue next byte
-	}
-	bytesRemaining--;
+  // If LED data is not full
+  if (outPos < sizeof(leds)){
+    dataSet();
+  }
+  bytesRemaining--;
  
-	if(bytesRemaining == 0) {
-		// End of data -- issue latch:
-		mode = Header; // Begin next header search
-		FastLED.show();
-		D_FPS;
-		D_LED(OFF);
-		SERIAL_FLUSH;
-	}
+  if(bytesRemaining == 0) {
+    // End of data -- issue latch:
+    mode = Header; // Begin next header search
+    FastLED.show();
+    D_FPS;
+    D_LED(OFF);
+    #ifdef SERIAL_FLUSH
+      serialFlush();
+    #endif
+  }
 }
 
-void timeouts(){
-	// No data received. If this persists, send an ACK packet
-	// to host once every second to alert it to our presence.
-	if((t - lastAckTime) >= 1000) {
-		Serial.print("Ada\n"); // Send ACK string to host
-		lastAckTime = t; // Reset counter
+void dataSet(){
+  #ifdef CALIBRATE
+    if(outPos < 3)
+      ledsRaw[outPos++] = c;
+    else{
+      ledsRaw[outPos] = ledsRaw[outPos%3]; // Sets RGB data to first LED color
+      outPos++;
+    }
+  #else
+    ledsRaw[outPos++] = c; // Issue next byte
+  #endif
+}
 
-		// If no data received for an extended time, turn off all LEDs.
-		if(SerialTimeout != 0 && (t - lastByteTime) >= (uint32_t) SerialTimeout * 1000) {
-			memset(leds, 0, Num_Leds * sizeof(struct CRGB)); //filling Led array by zeroes
-			FastLED.show();
-			mode = Header;
-			lastByteTime = t; // Reset counter
-		}
-	}
+
+
+void timeouts(){
+  // No data received. If this persists, send an ACK packet
+  // to host once every second to alert it to our presence.
+  if((t - lastAckTime) >= 1000) {
+    Serial.print("Ada\n"); // Send ACK string to host
+    lastAckTime = t; // Reset counter
+    ser_task();
+    // If no data received for an extended time, turn off all LEDs.
+    if(SerialTimeout != 0 && (t - lastByteTime) >= (uint32_t) SerialTimeout * 1000) {
+      mode = Header;
+      lastByteTime = t; // Reset counter
+    }
+    if (!idle_animation) {
+      memset(leds, 0, Num_Leds * sizeof(struct CRGB)); //filling Led array by zeroes
+      FastLED.show();
+    }
+    idle_animation = true;
+  }
+}
+
+
+void ser_task() {
+  EVERY_N_MILLISECONDS(60) {
+    nblendPaletteTowardPalette(currentPalette, targetPalette, maxChanges);  // Blend towards the target palette
+  }
+
+  EVERY_N_MILLISECONDS(50) {
+    serendipitous();     
+  }
+
+  LEDS.show();
+}
+
+void serendipitous () {
+
+  EVERY_N_SECONDS(5) {
+    uint8_t baseC = random8();
+    targetPalette = CRGBPalette16(CHSV(baseC-3, 255, random8(192,255)), CHSV(baseC+2, 255, random8(192,255)), CHSV(baseC+5, 192, random8(192,255)), CHSV(random8(), 255, random8(192,255)));
+
+    X = Xorig;
+    Y = Yorig;    
+  }
+
+//  Xn = X-(Y/2); Yn = Y+(Xn/2);
+//  Xn = X-Y/2;   Yn = Y+Xn/2;
+//  Xn = X-(Y/2); Yn = Y+(X/2.1);
+  Xn = X-(Y/3); Yn = Y+(X/1.5);
+//  Xn = X-(2*Y); Yn = Y+(X/1.1);
+  
+  X = Xn;
+  Y = Yn;
+
+  index=(sin8(X)+cos8(Y))/2;                            // Guarantees maximum value of 255
+
+  CRGB newcolor = ColorFromPalette(currentPalette, index, 255, LINEARBLEND);
+  
+//  nblend(leds[X%Num_Leds-1], newcolor, 224);          // Try and smooth it out a bit. Higher # means less smoothing.
+  nblend(leds[map(X,0,65535,0,Num_Leds)], newcolor, 224); // Try and smooth it out a bit. Higher # means less smoothing.
+  
+  fadeToBlackBy(leds, Num_Leds, 16);                    // 8 bit, 1 = slow, 255 = fast
+
+} // serendipitous()
+
+
+void serialFlush(){
+  while(Serial.available() > 0) {
+    Serial.read();
+  }
 }
